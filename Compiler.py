43174fea3464d07b582d7240847f216009594ea9
Vjs Pranav <pranavasri@live.in>
2020-03-28 03:38:21 +0530

    Use Conversation Handler to make the code interactive.
    Use Conversation Handler to make the code interactive and also add support for inputs

--- Compiler.py
+++ Compiler.py
@@ -4,14 +4,27 @@ Created on Thu Mar 12 18:57:28 2020
 
 @author: vjspranav
 """
+import logging
 
 import telegram
 import subprocess
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 TOKEN='Your Token ID here'
+bot = telegram.Bot(token=TOKEN)
+print(bot.get_me())
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+GET_CODE_C, GET_INP_C = range(2)
+
+TOKEN='1043028243:AAEWQvwm6V_9WHZ8qHd8v4crn_xdYfgNge0'
 bot = telegram.Bot(token=TOKEN)
 print(bot.get_me())
 updater = Updater(token=TOKEN, use_context=True)
@@ -20,44 +33,73 @@ dispatcher = updater.dispatcher
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, I'll help you compile all your codes trust me!")
 
-
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-
 updater.start_polling()
 
-def c(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=y)
-    c_code = update.message.text[2:]
-    f=open("R:/test.c","w+")
+def c2(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="OK give me some c code to execute")
+    return GET_CODE_C
+
+
+def get_code_c(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="entered this function ")
+    c_code = update.message.text
+    f=open("test.c","w+")
     f.write(c_code)
     f.close()
-    if(subprocess.call("gcc R:/test.c -o R:/test.exe", shell="True") ==0 ):
-        subprocess.call("R:/test.exe > R:/temp.txt", shell="True")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Please enter inputs(if any) or send 0 ")
+    return GET_INP_C    
+    
+def get_inp_c(update, context):
+    inp = update.message.text
+    f=open("input.txt","w+")
+    f.write(inp)
+    f.close()    
+    if(subprocess.call("gcc test.c -o test.exe", shell="True") ==0 ):
+        subprocess.call("test.exe < input.txt > temp.txt", shell="True")
         y=''
-        f=open("R:/temp.txt","r")
+        f=open("temp.txt","r")
         for i in f.readlines():
             y = y + str(i)
         f.close()
         context.bot.send_message(chat_id=update.effective_chat.id, text=y)
     else:
-        process = subprocess.Popen(['gcc', "R:/test.c", '-o', 'R:/test.exe'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False)
+        process = subprocess.Popen(['gcc', "test.c", '-o', 'test.exe'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False)
         (x, _) = process.communicate()
         x = x.decode("utf-8")
         y = "There were some errors\n"
         y = y + x
-        y = y.replace("R:/test.c", "Code")
+        y = y.replace("test.c", "Code")
         context.bot.send_message(chat_id=update.effective_chat.id, text=y)
+    return ConversationHandler.END
+    
+    
+def done():
+    pass    
 
-c_handler = CommandHandler("c", c)
-dispatcher.add_handler(c_handler) 
-    
-    
+conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('c', c2)],
+
+        states={
+            GET_CODE_C: [MessageHandler(Filters.text, get_code_c)],
+            GET_INP_C: [MessageHandler(Filters.text, get_inp_c)],    
+        },
+
+        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)]
+    )
+
+dispatcher.add_handler(conv_handler)
+updater.start_polling()
+
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
+
+updater.idle()
+
 
 
 
