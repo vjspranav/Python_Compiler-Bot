432da82990850862812730a63d759d7ff1507c0f
Vjs Pranav <pranavasri@live.in>
2020-03-28 03:47:14 +0530

    Add support for compiling C++ Codes
    Add an unknown command check :)

--- Compiler.py
+++ Compiler.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar 12 18:57:28 2020
+Created on Sat Mar 28 03:14:42 2020
 
 @author: vjspranav
 """
@@ -8,13 +8,7 @@ import logging
 
 import telegram
 import subprocess
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-TOKEN='Your Token ID here'
-bot = telegram.Bot(token=TOKEN)
-print(bot.get_me())
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -22,9 +16,9 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-GET_CODE_C, GET_INP_C = range(2)
+GET_CODE_C, GET_INP_C, GET_CODE_CPP, GET_INP_CPP = range(4)
 
-TOKEN='1043028243:AAEWQvwm6V_9WHZ8qHd8v4crn_xdYfgNge0'
+TOKEN='Your Token Here'
 bot = telegram.Bot(token=TOKEN)
 print(bot.get_me())
 updater = Updater(token=TOKEN, use_context=True)
@@ -74,32 +68,67 @@ def get_inp_c(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text=y)
     return ConversationHandler.END
     
+def cpp(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="OK give me some c++ code to execute")
+    return GET_CODE_CPP
+
+
+def get_code_cpp(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="entered this function ")
+    cpp_code = update.message.text
+    f=open("test.cpp","w+")
+    f.write(cpp_code)
+    f.close()
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Please enter inputs(if any) or send 0 ")
+    return GET_INP_CPP    
+    
+def get_inp_cpp(update, context):
+    inp = update.message.text
+    f=open("input.txt","w+")
+    f.write(inp)
+    f.close()    
+    if(subprocess.call("g++ test.cpp -o test.exe", shell="True") ==0 ):
+        subprocess.call("test.exe < input.txt > temp.txt", shell="True")
+        y=''
+        f=open("temp.txt","r")
+        for i in f.readlines():
+            y = y + str(i)
+        f.close()
+        context.bot.send_message(chat_id=update.effective_chat.id, text=y)
+    else:
+        process = subprocess.Popen(['g++', "test.cpp", '-o', 'test.exe'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False)
+        (x, _) = process.communicate()
+        x = x.decode("utf-8")
+        y = "There were some errors\n"
+        y = y + x
+        y = y.replace("test.cpp", "Code")
+        context.bot.send_message(chat_id=update.effective_chat.id, text=y)
+    return ConversationHandler.END
     
 def done():
     pass    
 
+
 conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('c', c2)],
+        entry_points=[CommandHandler('c', c2), CommandHandler('cpp', cpp)],
 
         states={
             GET_CODE_C: [MessageHandler(Filters.text, get_code_c)],
             GET_INP_C: [MessageHandler(Filters.text, get_inp_c)],    
+            GET_CODE_CPP: [MessageHandler(Filters.text, get_code_cpp)],
+            GET_INP_CPP: [MessageHandler(Filters.text, get_inp_cpp)],    
         },
 
         fallbacks=[MessageHandler(Filters.regex('^Done$'), done)]
     )
 
-dispatcher.add_handler(conv_handler)
-updater.start_polling()
-
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
 
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
 
+dispatcher.add_handler(conv_handler)
+updater.start_polling()
 updater.idle()
-
-
-
-
